3df00323271fc4e1b27ff09b16a8278bfb6ae6b4
Bconrad34 <bconrad1217@gmail.com>
2021-02-10 13:11:02 -0500

    Added basic crypto and currency input, CoinGeckoAPI

--- fomo_calculator_project/calculator/views.py
+++ fomo_calculator_project/calculator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pycoingecko import CoinGeckoAPI
+import json
+cg = CoinGeckoAPI()
 
 # Create your views here.
 def main(requests):
-    return HttpResponse("Hey Buddy")
+    coin=input("Enter your crypto: ")
+    currency=input("Enter your currency: ")
+    return HttpResponse(cg.get_price(ids=coin, vs_currencies=currency)[coin][currency])
