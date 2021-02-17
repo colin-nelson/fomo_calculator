6e210968fd9b35ac845e507bb060531b2b3feb71
Bconrad34 <bconrad1217@gmail.com>
2021-02-16 23:04:21 -0500

    Changed secret key

--- fomo_calculator_project/project/settings.py
+++ fomo_calculator_project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$f=(tvj@pe5qr!#vl0+kqm5_ez^y09d&0qbq_uv%=0nk_cbg8c'
+SECRET_KEY = variables.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
