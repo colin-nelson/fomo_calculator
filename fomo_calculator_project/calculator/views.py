176e9f6c2072a51072c1148def613995e1401583
colin-nelson <colinnelson9834@gmail.com>
2021-02-17 00:26:49 -0500

    added fomo functionality

--- fomo_calculator_project/calculator/views.py
+++ fomo_calculator_project/calculator/views.py
@@ -7,5 +7,20 @@ cg = CoinGeckoAPI()
 # Create your views here.
 def main(requests):
     coin=input("Enter your crypto: ")
-    currency=input("Enter your currency: ")
-    return HttpResponse(cg.get_price(ids=coin, vs_currencies=currency)[coin][currency])
+    date=input("Enter your date: ")
+    return HttpResponse(getFOMO(coin, date))
+
+def getPrice(coin):
+    return cg.get_price(ids=coin, vs_currencies='usd')[coin]['usd']
+
+def getHist(coin, date):
+    return cg.get_coin_history_by_id(coin, date)['market_data']['current_price']['usd']
+
+def getFOMO(coin, date):
+    buy = float(input("How much did you buy? "))
+    if getHist(coin, date) > buy:
+        numCoins = getHist(coin, date) / buy
+    else:
+        numCoins = buy / getHist(coin, date)
+
+    return (getPrice(coin) - getHist(coin, date)) * numCoins
