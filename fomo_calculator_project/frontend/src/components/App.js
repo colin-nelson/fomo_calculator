39696d59a742b3bde9c71cfc9431d737d5980c3d
Bconrad34 <bconrad1217@gmail.com>
2021-02-19 13:36:29 -0500

    React routing to different pages updated

--- fomo_calculator_project/frontend/src/components/App.js
+++ fomo_calculator_project/frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
 
 export default class App extends Component {
     constructor(props){
@@ -7,7 +9,11 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Home Page</h1>;
+        return (
+        <div>
+            <HomePage />
+        </div>
+        );
     }
 }
 
